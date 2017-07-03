57e42fae761d1ec16b6d1cfe00c515652cd6360e
Adrian Franz <adrian_franz@gmx.de>
2017-07-03 18:57:50 +0200

    added graphics

--- Graphics.Contracts/ILightSwitch.cs
+++ Graphics.Contracts/ILightSwitch.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Render.Contracts
+﻿namespace Graphics.Contracts
 {
     public interface ILightSwitch
     {
--- Graphics.Contracts/Polygon.cs
+++ Graphics.Contracts/Polygon.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 
-namespace Render.Contracts
+namespace Graphics.Contracts
 {
     public class Polygon
     {
