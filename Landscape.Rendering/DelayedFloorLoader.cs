813beb9165dfc23404cb20a64307afdaf7896559
eingeschränkt <eingeschrnkt@LAPTOP-AT7A8E2C>
2017-07-23 19:28:49 +0200

    added loader

--- Landscape.Rendering/DelayedFloorLoader.cs
+++ Landscape.Rendering/DelayedFloorLoader.cs
@@ -2,6 +2,7 @@
 using Graphics.Contracts;
 using Math.Contracts;
 using System.Collections.Generic;
+using System.Linq;
 using World.Model;
 
 namespace Landscape.Rendering
@@ -14,41 +15,65 @@ namespace Landscape.Rendering
         private uint _indexBufferId;
         private int _numberOfIndices;
 
+        private int _numberOfRows;
+        private int _metersPerTriangleSide;
+
+        private class FieldVertices
+        {
+            public FieldCoordinates Field { set; get; }
+            public float[] Vertices { set; get; }
+        }
+
+        private List<FieldCoordinates> _fieldQueue = new List<FieldCoordinates>();
+        private List<FieldVertices> _fieldVertexQueue = new List<FieldVertices>();
+
         public DelayedFloorLoader(IBufferObjectFactory bufferObjectFactory,
             IHeightCalculator heightCalculator,
             IFloorCollection floorCollection,
-            int sideLengthInMeters, 
-            int metersPerTriangleSide, 
-            int startx, 
-            int startz)
+            int numberOfRows, 
+            int metersPerTriangleSide)
         {
             _bufferObjectFactory = bufferObjectFactory;
             _heightCalculator = heightCalculator;
             _floorCollection = floorCollection;
+            _numberOfRows = numberOfRows;
+            _metersPerTriangleSide = metersPerTriangleSide;
 
-            ushort[] indices = CreateStandardIndexMesh(sideLengthInMeters / metersPerTriangleSide);
+            ushort[] indices = CreateStandardIndexMesh(_numberOfRows);
             _numberOfIndices = indices.Length;
             _indexBufferId = _bufferObjectFactory.GenerateIndexBuffer(indices);
         }
 
-        public void UpdateMesh(IEnumerable<FieldCoordinates> fields)
+        public void UpdateMesh(IEnumerable<FieldCoordinates> addedFields, IEnumerable<FieldCoordinates> removedFields)
         {
-            foreach(FieldCoordinates field in fields)
+            foreach(FieldCoordinates field in removedFields)
             {
-
+                _floorCollection.RemoveFloorPart(field.ID);
             }
-        }
 
-        private BufferedMeshUnit CreateRelativeHeightMapUnit(int numberOfRows, int metersPerTriangleSide, int startx, int startz)
-        {
-            float[] vertices = CreateVertices(numberOfRows, metersPerTriangleSide, startx, startz);
+            if(addedFields.Count() > 0)
+                _fieldQueue.AddRange(addedFields);
 
-            return new BufferedMeshUnit
+            if (_fieldVertexQueue.Count > 0)
             {
-                IndexBufferId = _indexBufferId,
-                NumberOfIndices = _numberOfIndices,
-                VertexBufferId = _bufferObjectFactory.GenerateVertexBuffer(vertices)
-            };
+                FieldVertices fieldVertices = _fieldVertexQueue.ElementAt(0);
+                _fieldVertexQueue.RemoveAt(0);
+
+                BufferedMeshUnit bufferedMeshUnit = new BufferedMeshUnit
+                {
+                    IndexBufferId = _indexBufferId,
+                    NumberOfIndices = _numberOfIndices,
+                    VertexBufferId = _bufferObjectFactory.GenerateVertexBuffer(fieldVertices.Vertices)
+                };
+                _floorCollection.AddFloorPart(fieldVertices.Field.ID, bufferedMeshUnit);
+            }
+            else if (_fieldQueue.Count > 0)
+            {
+                FieldCoordinates field = _fieldQueue.ElementAt(0);
+                _fieldQueue.RemoveAt(0);
+                float[] vertices = CreateVertices(_numberOfRows, _metersPerTriangleSide, field.X * _metersPerTriangleSide, field.Z * _metersPerTriangleSide);
+                _fieldVertexQueue.Add(new FieldVertices { Field = field, Vertices = vertices });
+            }
         }
 
         private float[] CreateVertices(int numberOfRows, int metersPerTriangleSide, int startx, int startz)
