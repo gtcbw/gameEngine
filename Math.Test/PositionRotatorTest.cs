57cfbd4fcd606bf14d39776bc23eaa23ab38e241
Adrian Franz <adrian_franz@gmx.de>
2017-10-01 14:41:40 +0200

    added rotator

---
+++ Math.Test/PositionRotatorTest.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Math.Test
+{
+    [TestClass]
+    public class PositionRotatorTest
+    {
+        [TestMethod]
+        public void Rotation180Degree()
+        {
+            PositionRotator positionRotator = new PositionRotator();
+
+            double[] result = positionRotator.Rotate(0, 0, 1, 180);
+
+            Assert.IsTrue(result[0] > -0.000001);
+            Assert.IsTrue(result[0] < 0.000001);
+
+            Assert.IsTrue(result[1] > -0.000001);
+            Assert.IsTrue(result[1] < 0.000001);
+
+            Assert.IsTrue(result[2] < -0.99999);
+            Assert.IsTrue(result[2] > -1.000001);
+        }
+
+        [TestMethod]
+        public void Rotation90Degree()
+        {
+            PositionRotator positionRotator = new PositionRotator();
+
+            double[] result = positionRotator.Rotate(0, 0, 1, 90);
+
+            Assert.IsTrue(result[0] > 0.99999);
+            Assert.IsTrue(result[0] < 1.000001);
+
+            Assert.IsTrue(result[1] > -0.000001);
+            Assert.IsTrue(result[1] < 0.000001);
+
+            Assert.IsTrue(result[2] > -0.000001);
+            Assert.IsTrue(result[2] < 0.000001);
+        }
+
+        [TestMethod]
+        public void Rotation45Degree()
+        {
+            PositionRotator positionRotator = new PositionRotator();
+
+            double[] result = positionRotator.Rotate(0, 0, 1, 45);
+
+            Assert.IsTrue(result[0] > 0.705);
+            Assert.IsTrue(result[0] < 0.709);
+
+            Assert.IsTrue(result[1] > -0.000001);
+            Assert.IsTrue(result[1] < 0.000001);
+
+            Assert.IsTrue(result[2] > 0.705);
+            Assert.IsTrue(result[2] < 0.709);
+        }
+
+        [TestMethod]
+        public void RotationNegative180Degree()
+        {
+            PositionRotator positionRotator = new PositionRotator();
+
+            double[] result = positionRotator.Rotate(0, 0, 1, -180);
+
+            Assert.IsTrue(result[0] > -0.000001);
+            Assert.IsTrue(result[0] < 0.000001);
+
+            Assert.IsTrue(result[1] > -0.000001);
+            Assert.IsTrue(result[1] < 0.000001);
+
+            Assert.IsTrue(result[2] < -0.99999);
+            Assert.IsTrue(result[2] > -1.000001);
+        }
+
+        [TestMethod]
+        public void Rotation540Degree()
+        {
+            PositionRotator positionRotator = new PositionRotator();
+
+            double[] result = positionRotator.Rotate(0, 0, 1, 540);
+
+            Assert.IsTrue(result[0] > -0.000001);
+            Assert.IsTrue(result[0] < 0.000001);
+
+            Assert.IsTrue(result[1] > -0.000001);
+            Assert.IsTrue(result[1] < 0.000001);
+
+            Assert.IsTrue(result[2] < -0.99999);
+            Assert.IsTrue(result[2] > -1.000001);
+        }
+
+        [TestMethod]
+        public void RotationFrom45To90Degree()
+        {
+            PositionRotator positionRotator = new PositionRotator();
+
+            double[] result = positionRotator.Rotate(0.70710678118, 0, 0.70710678118, 45);
+
+            Assert.IsTrue(result[0] > 0.99999);
+            Assert.IsTrue(result[0] < 1.000001);
+
+            Assert.IsTrue(result[1] > -0.000001);
+            Assert.IsTrue(result[1] < 0.000001);
+
+            Assert.IsTrue(result[2] > -0.000001);
+            Assert.IsTrue(result[2] < 0.000001);
+        }
+    }
+}
---
+++ Math/PositionRotator.cs
@@ -0,0 +1,21 @@
+﻿using OpenTK;
+
+namespace Math
+{
+    public sealed class PositionRotator
+    {
+        public double[] Rotate(double x, double y, double z, double rotationXZ)
+        {
+            double angleXZ = rotationXZ / 180.0 * System.Math.PI;
+
+            Matrix4d matrixRotation = Matrix4d.CreateRotationY(angleXZ);
+            //Matrix4d matrixTranslation = Matrix4d.CreateTranslation(0, 0, 0);
+            //Matrix4d fullMatrix = Matrix4d.Mult(matrixTranslation, matrixRotation);
+
+            Vector3d vector = Vector3d.Transform(new Vector3d(x, y, z), matrixRotation);
+
+
+            return new[] { vector.X, vector.Y, vector.Z };
+        }
+    }
+}
